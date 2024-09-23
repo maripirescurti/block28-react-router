c0356d75d10db51bad424f3d91847aba30899d6c
Mariana Pires Curti <marianapirescurti@MacBook-Pro.lan>
2024-09-22 23:12:42 -0400

    added navbar components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Blue from './components/Blue';
+import Red from './components/Red';
+import Home from './components/Home';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [] = useState(null);
 
+  
   return (
     <>
-      <div>
-        <h1>Hello React Router!</h1>
-        <div id="navbar">{/* navigation here */}</div>
-        <div id="main-section">{/* routes here */}</div>
+    <div id="container">
+      <div id="navbar">
+        <Link to="/blue">Blue</Link>
+        <Link to="/red">Red</Link>
+        <Link to="/home">Home</Link>
       </div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/blue' element={<Blue />}></Route>
+          <Route path='/red' element={<Red />}></Route>
+          <Route path='/home' element={<Home />}></Route>
+        </Routes>
+      </div>
+    </div>
     </>
   )
 }
-
-export default App
